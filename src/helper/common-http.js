51072c1939ef68150bb53a2d70dcff0bb3863c91
muham <muhammadhassan7788@gmail.com>
2022-05-21 17:16:34 +0700

    Commit

--- src/helper/common-http.js
+++ src/helper/common-http.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
-import URI from '../helper/uri-list.js';
-
 export default axios.create({
-    baseURL: `${URI.baseUri}/api/v1`,
+    baseURL: `${process.env.BACKEND_URI}/api/v1`,
     headers: {
         'Content-Type': 'application/json'
     }
--- src/helper/uri-list.js
+++
@@ -1,5 +0,0 @@
-class URI{
-    baseUri = 'http://localhost:81';
-}
-
-export default new URI();
